488c5f69c9fc00aeacaa6bfdd350ec557c290b0b
Auto-version-incrementer <none@none.com>
2022-04-07 12:48:16 +0100

    first draft of complete code

--- scripts/main.py
+++ scripts/main.py
@@ -2,6 +2,7 @@ from enum import Enum
 from pathlib import Path
 from typing import List
 
+import json
 import requests
 from requests.models import Response
 from requests import HTTPError
@@ -9,9 +10,10 @@ from rdflib import Graph
 from rdflib.plugins.sparql.processor import SPARQLResult
 from rdflib.plugins.sparql.sparql import FrozenBindings
 
-RESOURCE_URLS = [
-    'http://qudt.org/2.1/vocab/unit'
-]
+RESOURCE_URLS = {
+    'http://qudt.org/2.1/vocab/unit' : 'units.json',
+    'http://qudt.org/2.1/vocab/quantitykind' : 'quantity-kinds.json',
+}
 
 
 class SPARQL_QUERIES(Enum):
@@ -23,6 +25,12 @@ class SPARQL_QUERIES(Enum):
     SELECT ?unit {
         ?unit a <http://qudt.org/schema/qudt/Unit>
     }
+    """
+
+    IS_A_QUDT_QUANTITY_KIND = """
+    SELECT ?quantitykind (
+        ?quantitykind a <http://qudt.org/schema/qudt/QuantityKind> 
+    )
     """
 
 
@@ -43,6 +51,18 @@ def update_json(json_name: str, url_list: List[str]):
     list that are not already present.
     """
 
+    json_file = Path(__name__).parent/json_name
+
+    with open(json_file, "r") as jsonFile:
+        data = json.load(jsonFile)
+
+        #data['enum'].append(url_list)
+        for url in url_list:
+            #data['uris']["enum"].append(url)
+            data['enum'] += url
+
+    with open(json_file, "w") as jsonFile:
+        json.dump(data, jsonFile)
     # Read in the target json file
 
     # Add any urls from url_list that are not in the enum field within the json file
@@ -51,7 +71,7 @@ def update_json(json_name: str, url_list: List[str]):
 
 
 
-for resource_url in RESOURCE_URLS:
+for resource_url, json_name in RESOURCE_URLS.items():
     
     response: Response = get(resource_url)
 
@@ -62,4 +82,4 @@ for resource_url in RESOURCE_URLS:
 
     resource_urls = [str(x.get('unit')) for x in results.bindings]
 
-    update_json('units.json', resource_urls)
+    update_json(json_name, resource_urls)
