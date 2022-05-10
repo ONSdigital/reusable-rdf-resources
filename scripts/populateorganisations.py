7ef54de0f69114787b81d14a2599d8e024f60b60
Michael Adams <mikelovesbooks@gmail.com>
2022-05-10 09:00:18 +0100

    Issue #8 - Supporting organisation definitions.

---
+++ scripts/populateorganisations.py
@@ -0,0 +1,46 @@
+"""
+Populate Organisations
+----------------------
+
+Run this script from the top level of the repository to populate the `organisations.json` with the URIs defined in 
+ `rdf-definitions/organisations/organisations.csv`.
+
+"""
+
+from pathlib import Path
+import json
+import logging
+
+import pandas as pd
+
+_column_name_to_extract = "Identifier (registered site)"
+
+logger = logging.getLogger(__name__)
+
+
+def populate_organisations(csv_input: Path, json_output: Path) -> None:
+    """
+    Populate the JSON schema enum definition in :obj:`json_output` with the Organisation URIs defined
+      in :obj:`csv_input`.
+    """
+
+    organisation_uris = list(pd.read_csv(csv_input)[_column_name_to_extract])
+
+    logger.debug("Acquired organisations from CSV file.")
+
+    with open(json_output, "r") as f:
+        file_contents = json.load(f)
+
+        logger.debug(f"Loaded JSON from {json_output}")
+
+        file_contents["uris"]["enum"] = organisation_uris
+
+    with open(json_output, "w") as f:
+        logger.debug(f"Writing JSON to {json_output}")
+        json.dump(file_contents, f, indent=4)
+    
+    logger.debug("Done")
+
+
+if __name__ == "__main__":
+    populate_organisations(Path("rdf-definitions") / "organisations" / "organisations.csv", Path("organisations.json"))
